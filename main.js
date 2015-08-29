801c7eb68d48bc550c24d78556f14e4c33bd2ea7
ntinder <nathan.tinder@gmail.com>
2015-08-29 11:38:27 -0400

    smoothscroll listener specific to nav

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
-	console.log($('nav').height())
-    $('a').click(function(){
+    $('nav a').click(function(){
 	    $('html, body').animate({
 	        scrollTop: $( $.attr(this, 'href') ).offset().top - $('nav').height()
 	    }, 500);
