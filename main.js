512ce62d0cc0690e9abe26c806b2b7741e73d75c
ntinder <nathan.tinder@gmail.com>
2015-08-29 10:54:43 -0400

    adjust scroll offset for top nav

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 $(document).ready(function(){
+	console.log($('nav').height())
     $('a').click(function(){
 	    $('html, body').animate({
-	        scrollTop: $( $.attr(this, 'href') ).offset().top
+	        scrollTop: $( $.attr(this, 'href') ).offset().top - $('nav').height()
 	    }, 500);
 	    return false;
 	});
